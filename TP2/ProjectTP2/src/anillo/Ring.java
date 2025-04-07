b9a7c0a686e2e43c879a2ce2ccab536ed4503598
maxgubitosi <mgubitosi@udesa.edu.ar>
2025-04-07 17:44:44 -0300

    anillo naive
    - Clase completa
    - pasa test00NextOnEmpty
    - no me fije el resto todavía

--- TP2/ProjectTP2/src/anillo/Ring.java
+++ TP2/ProjectTP2/src/anillo/Ring.java
@@ -1,19 +1,60 @@
 package anillo;
 
 public class Ring {
+
+    private Node current;
+
+    private static class Node {
+        Object value;
+        Node next;
+
+        Node(Object value) {
+            this.value = value;
+            this.next = this; // apunta a si mismo (caso: un solo nodo en anillo)
+        }
+    }
+
+    public Ring() {  // Crear ring vacío
+        this.current = null;
+    }
+
     public Ring next() {
-        return null;
+        if (current != null) {current = current.next;}
+        if (current == null) {throw new IllegalStateException("Ring is empty");} // esta excepción es para que pase test00NextOnEmpty
+        return this;
     }
 
     public Object current() {
-        return null;
+        if (current == null) {return null;}
+        return current.value;
     }
 
     public Ring add( Object cargo ) {
-        return null;
+        if (current == null) {
+            current = new Node(cargo);
+        }
+        else {
+            Node newNode = new Node(cargo);
+            newNode.next = current.next;
+            current.next = newNode;
+            current = newNode;
+        }
+        return this;
     }
 
     public Ring remove() {
-        return null;
+        if (current == null) {return this;}
+        if (current.next == current) {  // caso: solo un nodo
+            current = null;
+        }
+        else {
+            Node prevNode = current;
+            while (prevNode.next != current) {  // da toda la vuelta al anillo hasta encontrar al anterior (no hay prev, solo next)
+                prevNode = prevNode.next;  // queda definido el prevNode correctamente
+            }
+            prevNode.next = current.next;
+            current = current.next;
+        }
+        return this;
     }
 }
