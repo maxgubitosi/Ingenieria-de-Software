a45deeff163ab38d162123923124ca44e8e1f009
maxgubitosi <mgubitosi@udesa.edu.ar>
2025-04-08 13:43:25 -0300

    anillo naive pasa todos los tests

--- TP2/ProjectTP2/src/anillo/Ring.java
+++ TP2/ProjectTP2/src/anillo/Ring.java
@@ -1,60 +1,60 @@
 package anillo;
 
 public class Ring {
-
-    private Node current;
-
-    private static class Node {
+    private Link current;
+    private static class Link {
         Object value;
-        Node next;
-
-        Node(Object value) {
-            this.value = value;
-            this.next = this; // apunta a si mismo (caso: un solo nodo en anillo)
-        }
+        Link next;
+        Link(Object v) { value = v; next = this; }
     }
 
-    public Ring() {  // Crear ring vacío
-        this.current = null;
+    public Ring() {
+        current = null;
+    }
+
+    public Ring add(Object cargo) {
+        Link newLink = new Link(cargo);
+        if (current == null) {
+            current = newLink;
+        } else {
+            Link prev = getPrev();
+            prev.next = newLink;
+            newLink.next = current;
+        }
+        current = newLink;
+        return this;
     }
 
     public Ring next() {
-        if (current != null) {current = current.next;}
-        if (current == null) {throw new IllegalStateException("Ring is empty");} // esta excepción es para que pase test00NextOnEmpty
+        if (current == null) throw new IllegalStateException("Ring is empty");
+        current = current.next;
         return this;
     }
 
     public Object current() {
-        if (current == null) {return null;}
+        if (current == null) throw new IllegalStateException("Ring is empty");
         return current.value;
     }
 
-    public Ring add( Object cargo ) {
+    public Ring remove() {
         if (current == null) {
-            current = new Node(cargo);
+            return this;
         }
-        else {
-            Node newNode = new Node(cargo);
-            newNode.next = current.next;
-            current.next = newNode;
-            current = newNode;
+        if (current.next == current) {
+            current = null;
+        } else {
+            Link prev= getPrev();
+            prev.next = current.next;
+            current = current.next;
         }
         return this;
     }
 
-    public Ring remove() {
-        if (current == null) {return this;}
-        if (current.next == current) {  // caso: solo un nodo
-            current = null;
+    private Link getPrev(){
+        Link prev = current;
+        while (prev.next != current) {
+            prev = prev.next;
         }
-        else {
-            Node prevNode = current;
-            while (prevNode.next != current) {  // da toda la vuelta al anillo hasta encontrar al anterior (no hay prev, solo next)
-                prevNode = prevNode.next;  // queda definido el prevNode correctamente
-            }
-            prevNode.next = current.next;
-            current = current.next;
-        }
-        return this;
+        return prev;
     }
 }
--- TP2/ProjectTP2/src/anillo/RingTest.java
+++ TP2/ProjectTP2/src/anillo/RingTest.java
@@ -1,6 +1,5 @@
 package anillo;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.time.LocalDate;
