3ea7cdfc8320a5141a833120e5846fda35f8d237
maxgubitosi <mgubitosi@udesa.edu.ar>
2025-06-09 11:02:25 -0300

    initial code tp4

---
+++ TP4/uno/src/uno_model/Draw2Card.java
@@ -0,0 +1,15 @@
+package uno_model;
+
+public class Draw2Card extends ColoredCard {
+    public static Card asCard( JsonCard aJson ) {
+        return new Draw2Card( aJson.getColor() ).shoutAs( aJson.isShout() );
+    }
+
+    public Draw2Card( String aColor ) { super( aColor ); }
+
+    public void affectGame( Match partida ) {
+        super.affectGame( partida );
+        partida.draw();
+        partida.draw();
+    }
+}
