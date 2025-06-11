a371d6c5c3b2a74258140da21b60f62c5e079153
Gabor Gorondi <ggorondi@udesa.edu.ar>
2025-06-11 11:15:57 -0300

    version 4b con gradle y spring etc

---
+++ TP4b/src/main/java/org/udesa/tp4b/Tp4bApplication.java
@@ -0,0 +1,13 @@
+package org.udesa.tp4b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4bApplication.class, args);
+    }
+
+}
---
+++ TP4b/src/test/java/org/udesa/tp4b/Tp4bApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesa.tp4b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
